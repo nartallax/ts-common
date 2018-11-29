0b9b78ce14b530b2f4e196df84f7ca80f43d533d
Nartallax <nartallax@gmail.com>
2018-11-29 04:08:34 +0300

    Custom binary format

--- net/http/client.ts
+++ net/http/client.ts
@@ -4,18 +4,44 @@ export interface Response {
 	data: string
 }
 
+export interface BinaryResponse {
+	data: ArrayBuffer
+}
+
+export interface RequestParams {
+	url: string;
+	method: string;
+	headers?: MapObject<string>;
+	body?: string | ArrayBuffer;
+}
+
 export class Client {
 	constructor(){}
 	
-	makeRequest(params: {url: string, method: string, headers?: MapObject<string>, body?: string}): Promise<Response> {
+	async makeRequest(params: RequestParams): Promise<Response> {
+		let res = await this.innerMakeRequest(params, false)
+		return {data: res.responseText}
+	}
+
+	async makeBinaryRequest(params: RequestParams): Promise<BinaryResponse> {
+		let res = await this.innerMakeRequest(params, true)
+		return {data: res.response}
+	}
+
+	private innerMakeRequest(params: RequestParams, binary: boolean): Promise<XMLHttpRequest> {
 		return new Promise((ok, bad) => {
 			try {
 				let req = new XMLHttpRequest();
 				
-				req.addEventListener("load", () => ok({data: req.responseText}));
+				req.addEventListener("load", () => ok(req));
 				req.addEventListener("error", x => bad(x));
 				req.open(params.method, params.url, true);
-				req.setRequestHeader("Content-type", "application/json");
+				
+				if(binary)
+					req.responseType = "arraybuffer";
+				else
+					req.setRequestHeader("Content-type", "application/json");
+
 				let headers = params.headers || {}
 				Object.keys(headers).forEach(hname => req.setRequestHeader(hname, headers[hname]));
 				
--- util/utf8.ts
+++ util/utf8.ts
@@ -47,13 +47,43 @@ export function getBytes(str: string): number[] {
 	return utf8;
 }
 
-export function getString(utf8: number[]): string {
-	var limit = utf8.length, 
+export function getBytesInto(str: string, destination: ArrayBuffer | DataView, start: number = 0): number {
+	let result = destination instanceof DataView? destination: new DataView(destination),
+		pos = start,
+		len = str.length;
+	for(var i = 0; i < len; i++) {
+		var charcode = str.charCodeAt(i);
+		if (charcode < 0x80){
+			result.setUint8(pos++, charcode)
+		} else if(charcode < 0x800){ 
+			result.setUint8(pos++, 0xc0 | (charcode >> 6));
+			result.setUint8(pos++, 0x80 | (charcode & 0x3f));
+		} else if(charcode < 0xd800 || charcode >= 0xe000) {
+			result.setUint8(pos++, 0xe0 | (charcode >> 12));
+			result.setUint8(pos++, 0x80 | ((charcode >> 6) & 0x3f));
+			result.setUint8(pos++, 0x80 | (charcode & 0x3f));
+		} else { // surrogate pair
+			i++;
+			// UTF-16 encodes 0x10000-0x10FFFF by
+			// subtracting 0x10000 and splitting the
+			// 20 bits of 0x0-0xFFFFF into two halves
+			charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+			result.setUint8(pos++, 0xf0 | (charcode >> 18));
+			result.setUint8(pos++, 0x80 | ((charcode >> 12) & 0x3f));
+			result.setUint8(pos++, 0x80 | ((charcode >> 6) & 0x3f));
+			result.setUint8(pos++, 0x80 | (charcode & 0x3f));
+		}
+	}
+	return pos - start;
+}
+
+export function getString(bytes: number[] | Uint8Array, start: number = 0, byteLength: number = -1): string {
+	var limit = byteLength < 0? bytes.length: start + byteLength, 
 		out = "", 
-		i = 0;
+		i = start;
 	var c1, c2, c3;
 	while(i < limit){
-		c1 = utf8[i++];
+		c1 = bytes[i++];
 		switch(c1 >> 4){ 
 			case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
 				// 0xxxxxxx
@@ -61,13 +91,44 @@ export function getString(utf8: number[]): string {
 				break;
 			case 12: case 13:
 				// 110x xxxx   10xx xxxx
-				c2 = utf8[i++];
+				c2 = bytes[i++];
 				out += String.fromCharCode(((c1 & 0x1F) << 6) | (c2 & 0x3F));
 				break;
 			case 14:
 				// 1110 xxxx  10xx xxxx  10xx xxxx
-				c2 = utf8[i++];
-				c3 = utf8[i++];
+				c2 = bytes[i++];
+				c3 = bytes[i++];
+				out += String.fromCharCode(((c1 & 0x0F) << 12) |
+				   ((c2 & 0x3F) << 6) |
+				   ((c3 & 0x3F) << 0));
+				break;
+		}
+	}
+	return out;
+}
+
+export function getStringFromBuffer(bytes: ArrayBuffer | DataView, start: number = 0, byteLength: number = -1): string {
+	let view = bytes instanceof DataView? bytes: new DataView(bytes);
+	var limit = byteLength < 0? view.byteLength: start + byteLength, 
+		out = "", 
+		i = start;
+	var c1, c2, c3;
+	while(i < limit){
+		c1 = view.getUint8(i++);
+		switch(c1 >> 4){ 
+			case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+				// 0xxxxxxx
+				out += String.fromCharCode(c1);
+				break;
+			case 12: case 13:
+				// 110x xxxx   10xx xxxx
+				c2 = view.getUint8(i++);
+				out += String.fromCharCode(((c1 & 0x1F) << 6) | (c2 & 0x3F));
+				break;
+			case 14:
+				// 1110 xxxx  10xx xxxx  10xx xxxx
+				c2 = view.getUint8(i++);
+				c3 = view.getUint8(i++);
 				out += String.fromCharCode(((c1 & 0x0F) << 12) |
 				   ((c2 & 0x3F) << 6) |
 				   ((c3 & 0x3F) << 0));
